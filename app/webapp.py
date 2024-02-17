dca2624c9eceef9b39293d240a31895780df4aa1
Алексей Прохоров <proch0r0v@yandex.ru>
2024-02-17 12:45:00 +0300

    My_first_homework_otus

---
+++ app/webapp.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict
+
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Homework",
+    description="Test FastApi application for Microservice Architecture",
+)
+
+
+
+@app.get("/")
+def get_index() -> str:
+    return "Hello,my first container"
+
+
+@app.get("/health")
+def get_health() -> Dict[str, Any]:
+    return {"status": "OK"}
